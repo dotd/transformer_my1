298587d9c616796d285e893bd4c9fe4da50a59cf
Dotan Di Castro <dotan.dicastro@gmail.com>
2023-01-01 17:55:49 +0200

    added medium implementation

--- medium_from_scratch/transformer.py
+++ medium_from_scratch/transformer.py
@@ -51,7 +51,7 @@ class MultiHeadAttention(nn.Module):
 def position_encoding(
         seq_len: int,
         dim_model: int,
-        device: torch.device) -> Tensor:
+        device: torch.device = "cpu") -> Tensor:
     pos = torch.arange(seq_len, dtype=torch.float, device=device).reshape(1, -1, 1)
     dim = torch.arange(dim_model, dtype=torch.float, device=device).reshape(1, 1, -1)
     phase = pos / (1e4 ** (dim // dim_model))
@@ -85,11 +85,11 @@ class Residual(nn.Module):
 
 class TransformerEncoderLayer(nn.Module):
     def __init__(
-        self,
-        dim_model: int = 512,
-        num_heads: int = 6,
-        dim_feedforward: int = 2048,
-        dropout: float = 0.1,
+            self,
+            dim_model: int = 512,
+            num_heads: int = 6,
+            dim_feedforward: int = 2048,
+            dropout: float = 0.1,
     ):
         super().__init__()
         dim_q = dim_k = max(dim_model // num_heads, 1)
@@ -111,12 +111,12 @@ class TransformerEncoderLayer(nn.Module):
 
 class TransformerEncoder(nn.Module):
     def __init__(
-        self,
-        num_layers: int = 6,
-        dim_model: int = 512,
-        num_heads: int = 8,
-        dim_feedforward: int = 2048,
-        dropout: float = 0.1,
+            self,
+            num_layers: int = 6,
+            dim_model: int = 512,
+            num_heads: int = 8,
+            dim_feedforward: int = 2048,
+            dropout: float = 0.1,
     ):
         super().__init__()
         self.layers = nn.ModuleList(
@@ -133,3 +133,100 @@ class TransformerEncoder(nn.Module):
             src = layer(src)
 
         return src
+
+
+class TransformerDecoderLayer(nn.Module):
+    def __init__(
+            self,
+            dim_model: int = 512,
+            num_heads: int = 6,
+            dim_feedforward: int = 2048,
+            dropout: float = 0.1,
+    ):
+        super().__init__()
+        dim_q = dim_k = max(dim_model // num_heads, 1)
+        self.attention_1 = Residual(
+            MultiHeadAttention(num_heads, dim_model, dim_q, dim_k),
+            dimension=dim_model,
+            dropout=dropout,
+        )
+        self.attention_2 = Residual(
+            MultiHeadAttention(num_heads, dim_model, dim_q, dim_k),
+            dimension=dim_model,
+            dropout=dropout,
+        )
+        self.feed_forward = Residual(
+            feed_forward(dim_model, dim_feedforward),
+            dimension=dim_model,
+            dropout=dropout,
+        )
+
+    def forward(self, tgt: Tensor, memory: Tensor) -> Tensor:
+        tgt = self.attention_1(tgt, tgt, tgt)
+        tgt = self.attention_2(tgt, memory, memory)
+        return self.feed_forward(tgt)
+
+
+class TransformerDecoder(nn.Module):
+    def __init__(
+            self,
+            num_layers: int = 6,
+            dim_model: int = 512,
+            num_heads: int = 8,
+            dim_feedforward: int = 2048,
+            dropout: float = 0.1,
+    ):
+        super().__init__()
+        self.layers = nn.ModuleList(
+            [
+                TransformerDecoderLayer(dim_model, num_heads, dim_feedforward, dropout)
+                for _ in range(num_layers)
+            ]
+        )
+        self.linear = nn.Linear(dim_model, dim_model)
+
+    def forward(self, tgt: Tensor, memory: Tensor) -> Tensor:
+        seq_len, dimension = tgt.size(1), tgt.size(2)
+        tgt += position_encoding(seq_len, dimension)
+        for layer in self.layers:
+            tgt = layer(tgt, memory)
+
+        return torch.softmax(self.linear(tgt), dim=-1)
+
+
+class Transformer(nn.Module):
+    def __init__(
+            self,
+            num_encoder_layers: int = 6,
+            num_decoder_layers: int = 6,
+            dim_model: int = 512,
+            num_heads: int = 6,
+            dim_feedforward: int = 2048,
+            dropout: float = 0.1,
+            activation: nn.Module = nn.ReLU(),
+    ):
+        super().__init__()
+        self.encoder = TransformerEncoder(
+            num_layers=num_encoder_layers,
+            dim_model=dim_model,
+            num_heads=num_heads,
+            dim_feedforward=dim_feedforward,
+            dropout=dropout,
+        )
+        self.decoder = TransformerDecoder(
+            num_layers=num_decoder_layers,
+            dim_model=dim_model,
+            num_heads=num_heads,
+            dim_feedforward=dim_feedforward,
+            dropout=dropout,
+        )
+
+    def forward(self, src: Tensor, tgt: Tensor) -> Tensor:
+        return self.decoder(tgt, self.encoder(src))
+
+
+src = torch.rand(64, 32, 512)
+tgt = torch.rand(64, 16, 512)
+out = Transformer()(src, tgt)
+print(out.shape)
+# torch.Size([64, 16, 512])
